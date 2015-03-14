782a871fdb69a532bf69dee3b152a9ac3edf477e
jamee.wang <jamee.wang@gmail.com>
2015-03-15 00:13:51 +0800

    changed load method

--- src/main/java/org/jamee/tool/webvisitabletester/WebVisitableTester.java
+++ src/main/java/org/jamee/tool/webvisitabletester/WebVisitableTester.java
@@ -1,17 +1,17 @@
 package org.jamee.tool.webvisitabletester;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.HashSet;
 import java.util.Random;
 import java.util.Set;
 
-import org.jsoup.Connection;
-import org.jsoup.Connection.Response;
-import org.jsoup.Jsoup;
-
 public class WebVisitableTester {
+    private int connectionTimeout = 5000; // 5s
+    private int readTimeout = 5000; // 5s
     private int maxTestDepth;
     private Set<String> requestedLinks;
     private boolean testExternalLink;
@@ -74,10 +74,46 @@ public class WebVisitableTester {
     }
 
     private int loadURL(final String url) throws IOException {
-        Connection conn = Jsoup.connect(url);
-        conn.execute();
-        Response response = conn.response();
-        return response.statusCode();
+        HttpURLConnection conn = null;
+        try {
+            conn = (HttpURLConnection) new URL(url).openConnection();
+            conn.setInstanceFollowRedirects(false); // don't rely on native redirection support
+            conn.setConnectTimeout(connectionTimeout);
+            conn.setReadTimeout(readTimeout);
+//            Host: www.baidu.com
+//            User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:36.0) Gecko/20100101 Firefox/36.0
+//            Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8
+//            Accept-Language: zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3
+//            Accept-Encoding: gzip, deflate
+            // Simulate Firefox Browser
+            conn.addRequestProperty("User-Agent",
+                    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:36.0) Gecko/20100101 Firefox/36.0");
+            conn.addRequestProperty("Accept","text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
+            conn.addRequestProperty("Accept-Language","zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3");
+            conn.addRequestProperty("Accept-Encoding","gzip, deflate");
+
+            conn.connect();
+            int contentLength = conn.getHeaderFieldInt("Content-Length", 1024* 1024);
+            byte[] buffer = new byte[1024];
+            int read = 0;
+            int remaining = contentLength;
+
+            InputStream inStream = conn.getInputStream();
+            while (remaining > 0) {
+                read = inStream.read(buffer);
+                if (read == -1) { break; }
+                remaining -= read;
+            }
+
+            return conn.getResponseCode();
+        } finally {
+            // per Java's documentation, this is not necessary, and precludes keepalives. However in practise,
+            // connection errors will not be released quickly enough and can cause a too many open files error.
+            if (null != conn) {
+                conn.disconnect();
+            }
+        }
+
     }
 
     private int getRequestCount(final LinkStatsEntry linkStatsEntry) {
@@ -125,8 +161,8 @@ public class WebVisitableTester {
     public static void main(String[] args) throws IOException {
         System.out
                 .println("Link\t\t\tTitle\tResponse Code\tRequested Count\tMax Response Time\tMin Response Time\tAvarage Response Time");
-        WebVisitableTester wvtester = new WebVisitableTester("http://www.baidu.com/", "baidu", 5, 1,
-                2, true);
+        WebVisitableTester wvtester = new WebVisitableTester("https://www.baidu.com/", "baidu", 1, 1,
+                1, false);
         wvtester.setLinkStatsStreamRender(new LinkStatsStreamRender() {
             public void render(LinkStatsEntry linkStatsEntry, int depth) {
                 for (int i = 0; i < depth; ++i)
